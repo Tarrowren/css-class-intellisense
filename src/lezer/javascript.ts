4b2efd233123961557c299be4ea85750b3e30e1d
Tarrowren <tarrowren@foxmail.com>
2023-02-10 18:35:01 +0800

    fix js

--- src/lezer/javascript.ts
+++ src/lezer/javascript.ts
@@ -1,9 +1,8 @@
 import * as LEZER_JS from "@lezer/javascript";
-
 const types = LEZER_JS.parser.nodeSet.types;
 
 export namespace JS_NODE_TYPE {
-  export const String = types[12],
-    Import = types[135],
-    ImportDeclaration = types[221];
+  export const String = types[11],
+    Import = types[139],
+    ImportDeclaration = types[225];
 }
