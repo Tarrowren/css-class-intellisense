8287ec5554d1aac01faa1223ff7c4f4d7167c815
Tarrowren <tarrowren@foxmail.com>
2023-02-27 14:53:44 +0800

    webpack watch

--- scripts/browser.mjs
+++ scripts/browser.mjs
@@ -1,11 +1,18 @@
+// @ts-check
+
+import { rm } from "fs/promises";
 import { resolve } from "path";
 import webpack from "webpack";
 
-const OUT_PATH = resolve("dist");
+const mode = process.env.NODE_ENV === "production" ? "production" : "none";
+const watch = !!process.env.WEBPACK_WATCH;
+
+await rm(resolve("dist", "browser"), { recursive: true, force: true });
 
 webpack(
   {
-    mode: "production",
+    mode,
+    watch,
     target: "webworker",
     entry: {
       "browser/main": resolve("src", "browser", "main.ts"),
@@ -22,19 +29,27 @@ webpack(
         },
       ],
     },
+    plugins: [new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })],
     externals: {
       vscode: "commonjs vscode",
     },
     output: {
-      path: OUT_PATH,
-      libraryTarget: "commonjs",
+      path: resolve("dist", "browser"),
+      filename: "main.js",
+      libraryTarget: "commonjs2",
+    },
+    devtool: mode === "production" ? false : "source-map",
+    infrastructureLogging: {
+      level: "log",
     },
   },
   (err, stats) => {
     if (err) {
       console.error(err.message);
     } else {
-      console.log(stats.toString({ chunks: false, colors: true }));
+      if (stats) {
+        console.log(stats.toString({ chunks: false, colors: true }));
+      }
     }
   }
 );
--- scripts/node.mjs
+++ scripts/node.mjs
@@ -1,11 +1,18 @@
+// @ts-check
+
+import { rm } from "fs/promises";
 import { resolve } from "path";
 import webpack from "webpack";
 
-const OUT_PATH = resolve("dist");
+const mode = process.env.NODE_ENV === "production" ? "production" : "none";
+const watch = !!process.env.WEBPACK_WATCH;
+
+await rm(resolve("dist", "node"), { recursive: true, force: true });
 
 webpack(
   {
-    mode: "production",
+    mode,
+    watch,
     target: "node",
     entry: {
       "node/main": resolve("src", "node", "main.ts"),
@@ -26,15 +33,22 @@ webpack(
       vscode: "commonjs vscode",
     },
     output: {
-      path: OUT_PATH,
-      libraryTarget: "commonjs",
+      path: resolve("dist", "node"),
+      filename: "main.js",
+      libraryTarget: "commonjs2",
+    },
+    devtool: mode === "production" ? false : "source-map",
+    infrastructureLogging: {
+      level: "log",
     },
   },
   (err, stats) => {
     if (err) {
       console.error(err.message);
     } else {
-      console.log(stats.toString({ chunks: false, colors: true }));
+      if (stats) {
+        console.log(stats.toString({ chunks: false, colors: true }));
+      }
     }
   }
 );
